023053b82761b41836f1653343353fe24812e24b
wanyu747839@outlook.com <wanyu747839@outlook.com>
2022-03-14 17:57:45 +0800

    spring mvc

---
+++ SpringMVC/SpringMVC_Demo1/src/main/java/com/springMvc/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.springMvc.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author wanyu
+ * @createTime 2022-03-14 17:53
+ */
+@Controller
+public class HelloController {
+
+}
