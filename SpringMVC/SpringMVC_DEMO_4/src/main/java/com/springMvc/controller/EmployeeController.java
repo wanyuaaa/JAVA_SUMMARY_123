805ca2cc530feeeea46721c5661926a9274ec235
wanyu <wanyu747839@outlook.com>
2022-03-16 01:46:11 +0800

    springmvc

--- SpringMVC/SpringMVC_DEMO_4/src/main/java/com/springMvc/controller/EmployeeController.java
+++ SpringMVC/SpringMVC_DEMO_4/src/main/java/com/springMvc/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.springMvc.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,5 +26,29 @@ public class EmployeeController {
         Collection<Employee> all = employeeDao.getAll();
         model.addAttribute("employeeList",all);
         return "employee";
+    }
+
+    @RequestMapping(value = "/employee/{id}",method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id")Integer id){
+        employeeDao.delete(id);
+        return "redirect:/employee";
+    }
+    @RequestMapping(value = "/employee/{id}",method = RequestMethod.GET)
+    public String getEmployeeById(@PathVariable("id")Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("employee",employee);
+        return "employee_update";
+    }
+
+    @RequestMapping(value = "/employee",method = RequestMethod.POST)
+    public String addEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+
+    @RequestMapping(value = "/employee",method = RequestMethod.PUT)
+    public String updateEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
     }
 }
