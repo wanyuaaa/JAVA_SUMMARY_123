b446390b2a4521ce7dcf8f7f7a6889c7a3a6be91
wanyu <wanyu747839@outlook.com>
2022-03-17 02:05:18 +0800

    mybatis

---
+++ MyBatis/MyBatis_DEMO_1/src/main/java/com/mybatis/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.mybatis.mapper;
+
+/**
+ * @author wanyu
+ * @createTime 2022-03-16 23:27
+ */
+public interface UserMapper {
+    //添加用户信息
+    int insertUser();
+}
---
+++ MyBatis/MyBatis_DEMO_1/src/main/java/com/mybatis/pojo/User.java
@@ -0,0 +1,74 @@
+package com.mybatis.pojo;
+
+/**
+ * @author wanyu
+ * @createTime 2022-03-16 23:25
+ */
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private Integer age;
+    private char sex;
+    private String email;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
